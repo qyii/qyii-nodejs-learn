aad6fce1c23e01a6f7dd2320ee6a76a01934bbbe
qyii <apupupupupu@gmail.com>
2018-01-08 11:26:08 +0800

    update server

--- build/requestHandlers.js
+++ build/requestHandlers.js
@@ -1,11 +1,20 @@
-function start() {
+var exec = require('child_process').exec
+
+function start(response) {
   console.log("Request handler 'start' was called.")
-  return 'Hello Start'
+
+  setTimeout(() => {
+    response.writeHead(200, { 'Content-Type': 'text/plain' })
+    response.write('hello stdout')
+    response.end()
+  }, 2000)
 }
 
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called.")
-  return 'Hello Upload'
+  response.writeHead(200, { 'Content-Type': 'text/plain' })
+  response.write('Hello Upload')
+  response.end()
 }
 
 exports.start = start
