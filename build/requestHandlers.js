71f9fee2bbfbafa0b263b56c3abbc4a03e5362a3
qyii <apupupupupu@gmail.com>
2018-01-08 13:57:18 +0800

    postData

--- build/requestHandlers.js
+++ build/requestHandlers.js
@@ -1,19 +1,28 @@
-var exec = require('child_process').exec
-
-function start(response) {
+function start(response, postData) {
   console.log("Request handler 'start' was called.")
 
-  setTimeout(() => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' })
-    response.write('hello stdout')
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>'
+
+    response.writeHead(200, { 'Content-Type': 'text/html' })
+    response.write(body)
     response.end()
-  }, 2000)
 }
 
-function upload(response) {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called.")
   response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.write('Hello Upload')
+  response.write("You've sent: " + postData)
   response.end()
 }
 
--- build/router.js
+++ build/router.js
@@ -1,10 +1,10 @@
-function route(handle, pathname, response) {
+function route(handle, pathname, response, postData) {
   // console.log('About to route a request for ' + pathname)
   if (pathname.indexOf('favicon') !== -1) {
     return 'icon'
   }
   if (typeof handle[pathname] === 'function') {
-    return handle[pathname](response)
+    return handle[pathname](response, postData)
   } else {
     console.log('404 not found')
     response.writeHead(404, { 'Content-Type': 'text/plain' })
