ee5115483b1e7773ea7509fd44993eaa0dd5b8f3
qyii <apupupupupu@gmail.com>
2018-01-08 16:54:31 +0800

    pic upload finish

--- build/requestHandlers.js
+++ build/requestHandlers.js
@@ -1,15 +1,20 @@
+const queryString = require('querystring')
+const fs = require('fs')
+const formidable = require('formidable')
+
 function start(response, postData) {
   console.log("Request handler 'start' was called.")
 
   var body = '<html>'+
     '<head>'+
-    '<meta http-equiv="Content-Type" content="text/html; '+
-    'charset=UTF-8" />'+
+    '<meta http-equiv="Content-Type" '+
+    'content="text/html; charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload">'+
+    '<input type="submit" value="Upload file" />'+
     '</form>'+
     '</body>'+
     '</html>'
@@ -19,12 +24,44 @@ function start(response, postData) {
     response.end()
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log("Request handler 'upload' was called.")
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.write("You've sent: " + postData)
-  response.end()
+  var form = new formidable.IncomingForm()
+  console.log('about to parse')
+  // form.uploadDir = './Static' // 设置上传的路径
+  form.parse(request, function(error, fields, files) {
+    console.log('parsing done' + files.upload.path)
+    // fs.renameSync(files.upload.path, './tmp/test.jpg')
+
+    // 解决跨分区操作的权限问题报错 cross-device link not permitted 采用filestream的形式
+    var readStream = fs.createReadStream(files.upload.path)
+    var writeStream = fs.createWriteStream('./tmp/test.jpg')
+    readStream.pipe(writeStream)
+    readStream.on('end', function() {
+      fs.unlinkSync(files.upload.path)
+    })
+    response.writeHead(200, { 'Content-Type': 'text/html' })
+    response.write('received image:<br/>')
+    response.write("<img src='/show' />")
+    response.end()
+  })
+}
+
+function show(response, postData) {
+  console.log("Request handler 'show' was called.")
+  fs.readFile('./tmp/test.jpg', 'binary', function(error, file) {
+    if (error) {
+      response.writeHead(500, { 'Content-Type': 'text/plain' })
+      response.write(error + '\n')
+      response.end()
+    } else {
+      response.writeHead(200, { 'Content-Type': 'image/png' })
+      response.write(file, 'binary')
+      response.end()
+    }
+  })
 }
 
 exports.start = start
 exports.upload = upload
+exports.show = show
--- build/router.js
+++ build/router.js
@@ -1,10 +1,10 @@
-function route(handle, pathname, response, postData) {
+function route(handle, pathname, response, request) {
   // console.log('About to route a request for ' + pathname)
   if (pathname.indexOf('favicon') !== -1) {
     return 'icon'
   }
   if (typeof handle[pathname] === 'function') {
-    return handle[pathname](response, postData)
+    return handle[pathname](response, request)
   } else {
     console.log('404 not found')
     response.writeHead(404, { 'Content-Type': 'text/plain' })
